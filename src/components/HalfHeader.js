6969fe149168545be9944dac562ee7ff6c72591a
dario elefante <dar.elefante@gmail.com>
2021-04-08 20:45:47 +0200

    inserita nuova card, inizializzato change header

--- src/components/HalfHeader.js
+++ src/components/HalfHeader.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
+const [header, setHeader] = useState();
+
+const changeHeader = () => {
+  const url = window.location.href;
+
+  if (url === "http://localhost:3000/Corsi") {
+    setHeader("I Corsi");
+  } else if (url === "http://localhost:3000/Prodotti") {
+    setHeader("I Prodotti");
+  }
+};
+changeHeader();
 const HalfHeader = () => {
   return (
     <section className="p-0 m-0">
       <div className="container-fluid d-flex flex-column justify-content-around half-above">
         <div className="row">
           <div data-aos="zoom-in-up" className="col-12">
-            <h1 className="mt-5 pt-5">Il progetto</h1>
+            <h1 className="mt-5 pt-5">{header}</h1>
             <p className=" mr-md-5 mt-3 py-md-5">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
               soluta ea esse iste eius, non illo aliquid atque hic nisi, quidem
--- src/components/Views/Prodotti.js
+++ src/components/Views/Prodotti.js
@@ -1,53 +1,28 @@
-import { CardDeck, Card } from "react-bootstrap";
 import HalfHeader from "../HalfHeader.js";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 
 function Prodotti() {
   return (
     <>
       <HalfHeader />
-      <CardDeck className="my-5 px-5">
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Prodotto 1</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Prodotto 2</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to
-              additional content.{" "}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Prodotto 3</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-      </CardDeck>
+      <Flippy
+        flipOnHover={false} // default false
+        flipOnClick={true} // default false
+        flipDirection="horizontal" // horizontal or vertical
+        href="https://picsum.photos/200/300" // to use toggle method like this.flippy.toggle()
+        // if you pass isFlipped prop component will be controlled component.
+        // and other props, which will go to div
+        style={{ width: "200px", height: "200px" }} /// these are optional style, it is not necessary
+      >
+        <FrontSide
+          style={{
+            backgroundColor: "#41669d",
+          }}
+        >
+          RICK
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }}>ROCKS</BackSide>
+      </Flippy>
     </>
   );
 }
