f325bc49e3874eac7d98452459eb1eed4d9752fd
dario elefante <dar.elefante@gmail.com>
2021-05-21 20:17:55 +0200

    Generato sistema interno per inserimento dati su Product

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,43 +1,41 @@
 import Flippy, { FrontSide, BackSide } from "react-flippy";
 
-function Prodotti() {
+function Prodotti({ product }) {
+  console.log(product.image);
   return (
-    <Flippy
-      flipOnHover={false} // default false
-      flipOnClick={true} // default false
-      flipDirection="horizontal" // horizontal or vertical
-      src="https://picsum.photos/200/300" // to use toggle method like this.flippy.toggle()
-      // if you pass isFlipped prop component will be controlled component.
-      // and other props, which will go to div
-      style={{ width: "300px", height: "400px" }} /// these are optional style, it is not necessary
-    >
-      <FrontSide
-        style={{
-          backgroundColor: "#41669d",
-        }}
-        className="p-0 mx-0 rounded "
+    <div className="my-5">
+      <Flippy
+        flipOnHover={false} // default false
+        flipOnClick={true} // default false
+        flipDirection="horizontal" // horizontal or vertical
+        src={product.image} // to use toggle method like this.flippy.toggle()
+        // if you pass isFlipped prop component will be controlled component.
+        // and other props, which will go to div
+        style={{ width: "300px", height: "400px" }} /// these are optional style, it is not necessary
       >
-        <div className="container-fluid m-0 p-0">
-          <img
-            className="img-fluid mx-auto"
-            src="https://picsum.photos/400/550"
-            alt=""
-          />
-        </div>
-      </FrontSide>
-      <BackSide style={{ backgroundColor: "#175852" }} className="rounded ">
-        {" "}
-        <div className="py-3">
-          <h4>Product description</h4>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
-            rerum, quo magni quis dolores repudiandae debitis in quidem quasi.
-            Nulla modi tempore sunt. Totam quae magnam aliquam error molestias
-            aut.
-          </p>
-        </div>
-      </BackSide>
-    </Flippy>
+        <FrontSide
+          style={{
+            backgroundColor: "#41669d",
+          }}
+          className="p-0 mx-0 rounded "
+        >
+          <div className="container-fluid m-0 p-0">
+            <img
+              src="public\media\alabarda_spaziale.jpg"
+              className="img-fluid mx-auto"
+              alt=""
+            />
+          </div>
+        </FrontSide>
+        <BackSide style={{ backgroundColor: "#175852" }} className="rounded ">
+          {" "}
+          <div className="py-3">
+            <h4 id="title">{product.name}</h4>
+            <p id="description">{product.description}</p>
+          </div>
+        </BackSide>
+      </Flippy>
+    </div>
   );
 }
 
--- src/components/Views/Prodotti.js
+++ src/components/Views/Prodotti.js
@@ -2,20 +2,27 @@ import HalfHeader from "../HalfHeader.js";
 import Cards from "../Cards";
 
 function Prodotti() {
+  const productCatalog = [
+    {
+      image: "publicmediaalabarda_spaziale.jpg",
+      name: "Alabarda Spaziale",
+      description: "Alabarda Spaziale",
+    },
+    {
+      image: "publicmediaalabarda_spaziale.jpg",
+      name: "Alabarda Spaziale",
+      description: "Alabarda Spaziale",
+    },
+  ];
+
   return (
     <>
       <HalfHeader />
       <div className="container my-5">
         <div className="row justify-content-around">
-          <div className="my-5">
-            <Cards />
-          </div>
-          <div className="my-5">
-            <Cards />
-          </div>
-          <div className="my-5">
-            <Cards />
-          </div>
+          {productCatalog.map((product) => (
+            <Cards id={product.name} product={product} />
+          ))}
         </div>
       </div>
     </>
